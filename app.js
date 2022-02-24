8225698ea4ca06e37f2dcd3832e83e144e429806
José Rodriguez <isc.joserodriguez@gmail.com>
2022-02-23 18:49:47 -0700

    Seleccionar producto e imprimir el precio

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log('Hola')
 let fin = false;
 
 while (fin == false) {
-  window.prompt(`
+  let index = window.prompt(`
   Elige un producto
   1.- Pizza básica, con un precio de 5 USD por cada una.
   2.- Pizza con 2 ingredientes, con un precio de 10 USD por cada una.
@@ -13,6 +13,22 @@ while (fin == false) {
   4.- Refresco chico, con un precio de 2 USD.
   5.- Refresco grande, con un precio de 3 USD.
   `)
-  console.log();
+  
+  if(index == '1'){
+    console.log('$5')
+  }
+  if(index == '2'){
+    console.log('$10')
+  }
+  if(index == '3'){
+    console.log('$12')
+  }
+  if(index == '4'){
+    console.log('$2')
+  }
+  if(index == '5'){
+    console.log('$3')
+  }
+
   fin = true;
 }
