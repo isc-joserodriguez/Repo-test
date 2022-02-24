0110d7e6b385060fe13f46194571e604b23a569c
José Rodriguez <isc.joserodriguez@gmail.com>
2022-02-23 18:57:50 -0700

    Terminado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('Hola')
 /* 
  */
 let fin = false;
+let suma=0;
 
 while (fin == false) {
   let index = window.prompt(`
@@ -13,22 +14,32 @@ while (fin == false) {
   4.- Refresco chico, con un precio de 2 USD.
   5.- Refresco grande, con un precio de 3 USD.
   `)
-  
-  if(index == '1'){
+
+  if (index == '1') {
     console.log('$5')
+    suma = suma + 5;
+    console.log('El total es: '+suma);
   }
-  if(index == '2'){
+  if (index == '2') {
     console.log('$10')
+    suma = suma + 10;
+    console.log('El total es: '+suma);
   }
-  if(index == '3'){
+  if (index == '3') {
     console.log('$12')
+    suma = suma + 12;
+    console.log('El total es: '+suma);
   }
-  if(index == '4'){
+  if (index == '4') {
     console.log('$2')
+    suma = suma + 2;
+    console.log('El total es: '+suma);
   }
-  if(index == '5'){
+  if (index == '5') {
     console.log('$3')
+    suma = suma + 3;
+    console.log('El total es: '+suma);
   }
 
-  fin = true;
+  fin = window.prompt(`¿Quiere agregar un nuevo producto?`) === 's' ? false : true;
 }
