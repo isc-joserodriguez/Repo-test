1f4a4c1b9eb1e9542803ec34fc2e4e122b22d1bf
José Rodriguez <isc.joserodriguez@gmail.com>
2022-02-23 18:47:23 -0700

    Funciona el seleccionar

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 console.log('Hola')
 
-/* Pizza básica, con un precio de 5 USD por cada una.
-Pizza con 2 ingredientes, con un precio de 10 USD por cada una.
-Pizza con 3 ingredientes, con un precio de 12 USD por cada una.
-Refresco chico, con un precio de 2 USD.
-Refresco grande, con un precio de 3 USD.
+/* 
  */
 let fin = false;
 
 while (fin == false) {
-  console.log('Elige un producto');
+  window.prompt(`
+  Elige un producto
+  1.- Pizza básica, con un precio de 5 USD por cada una.
+  2.- Pizza con 2 ingredientes, con un precio de 10 USD por cada una.
+  3.- Pizza con 3 ingredientes, con un precio de 12 USD por cada una.
+  4.- Refresco chico, con un precio de 2 USD.
+  5.- Refresco grande, con un precio de 3 USD.
+  `)
+  console.log();
   fin = true;
 }
