88822e2063f39b1b2985d82d89ced4d13401948e
José Rodriguez <isc.joserodriguez@gmail.com>
2022-02-23 18:42:22 -0700

    Inicio

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Hola')
+
+/* Pizza básica, con un precio de 5 USD por cada una.
+Pizza con 2 ingredientes, con un precio de 10 USD por cada una.
+Pizza con 3 ingredientes, con un precio de 12 USD por cada una.
+Refresco chico, con un precio de 2 USD.
+Refresco grande, con un precio de 3 USD.
+ */
